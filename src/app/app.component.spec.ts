6fec896f70e55c6f41dd27b2e30881cf152926b9
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2024-04-04 16:23:00 -0400

    chore: Move contents up one folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'one-time-payment' title`, () => {
+  it(`should have the 'one-time-loan-payment' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('one-time-payment');
+    expect(app.title).toEqual('one-time-loan-payment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, one-time-payment');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, one-time-loan-payment');
   });
 });
